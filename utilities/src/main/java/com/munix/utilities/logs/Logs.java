70333e8033781662fac9d6742e50f82948619178
jpmuniz <jpmuniz@gmail.com>
2016-03-01 09:57:43 +0100

    Test jitpack

---
+++ utilities/src/main/java/com/munix/utilities/logs/Logs.java
@@ -0,0 +1,13 @@
+package com.munix.utilities.logs;
+
+import android.util.Log;
+
+/**
+ * Created by munix on 1/03/16.
+ */
+public class Logs {
+
+    public static void test() {
+        Log.v( "Utilities", "TEST LOG" );
+    }
+}
